07ce8a0e61e92ba2202ad3d2332c3f7639783bed
Prasanta Kumar Khuntia <prasantakhuntia2018@gmail.com>
2026-08-06 12:07:17 +0530

    Done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,55 +1,67 @@
 import React from "react";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
+// Standardize component imports
 import Footer from "./components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
 import Topbar from "./Components/Topbar/Topbar";
+
 import Home from "./Pages/Home/Home";
+import AboutUs from "./Pages/AboutUs/AboutUs";
+
+// Education Routes
 import Education from "./Components/Education/Education";
-import BookDetails from "./Components/BookDetails/BookDetails";
+import BookDetails from "./Components/BookDetails/BookDetails"; // Serves as Education Details
+
+// Team Routes
 import OurTeam from "./Components/OurTeam/OurTeam";
 import TeamDetails from "./Components/TeamDetails/TeamDetails";
+
+// Blog Routes
 import Bloggrids from "./Pages/Bloggrids/Bloggrids";
 import Blogdetails from "./Pages/Blogdetails/Blogdetails";
+
+// Other Routes
 import ContactUs from "./Pages/ContactUs/ContactUs";
-import AboutUs from "./Pages/AboutUs/AboutUs";
 import FaqSection from "./Components/FaqSection/FaqSection";
-
-  
-
+// Option: Import a NotFound component if you have one
+// import NotFound from "./Pages/NotFound/NotFound";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Topbar/>
-     <Navbar/>
+      <Topbar />
+      <Navbar />
 
       <Routes>
-      <Route path="/" element={<Home/>}/>
-         <Route path="/Education" element={<Education/>} />
-         <Route path="/BookDetails" element={<BookDetails/>} />
-         <Route path="/OurTeam" element={<OurTeam/>} />
-         <Route path="/TeamDetails" element={<TeamDetails/>} />
+        {/* Homepage */}
+        <Route path="/" element={<Home />} />
 
-         <Route path='/about' element={<AboutUs/>}/>
-         <Route path='/blog' element={<Bloggrids />}/>
-         <Route path='/blogdetails' element={<Blogdetails />}/>
-         <Route path='/contact' element={<ContactUs />}/>
-         <Route path='/FaqSection' element={<FaqSection/>}/>
+        {/* About Page */}
+        <Route path="/about-us" element={<AboutUs />} />
 
+        {/* SEO-Optimized Education & Education Details Routes */}
+        <Route path="/education" element={<Education />} />
+        {/* Dynamic dynamic route for Education/Book details (use :slug or :id) */}
+        <Route path="/education/details" element={<BookDetails />} />
 
+        {/* Team Routes */}
+        <Route path="/our-team" element={<OurTeam />} />
+        <Route path="/our-team/details" element={<TeamDetails />} />
 
+        {/* Blog Routes */}
+        <Route path="/blog" element={<Bloggrids />} />
+        <Route path="/blog/details" element={<Blogdetails />} />
 
-        
-         
+        {/* Contact & FAQ */}
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/faqs" element={<FaqSection />} />
+
+        {/* 404 Catch-All for SEO (prevents broken indexing) */}
+        <Route path="*" element={<div>404 - Page Not Found</div>} />
       </Routes>
 
-     <Footer/>
+      <Footer />
     </BrowserRouter>
   );
 };
